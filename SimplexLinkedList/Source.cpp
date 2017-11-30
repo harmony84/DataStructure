663f51f4353e08e8320c4e94bf52531cad39a4f7
harmony84 <harmonic84@gmail.com>
2017-11-30 17:55:44 +0900

    Added Function

--- SimplexLinkedList/Source.cpp
+++ SimplexLinkedList/Source.cpp
@@ -1,121 +1,238 @@
 #include <iostream>
 #include <string>
+#include <crtdbg.h>
 
-
-struct tagList
+#pragma region "define node"
+struct tagNode
 {
-	tagList() {}
-	tagList(const std::string& s) : str(s) {} 
+	tagNode() { }
+	tagNode( std::string&& n ) : name( std::move( n ) ) { }
 
-	std::string str{ "None" };
-	tagList* next{ nullptr };
+	std::string name{"None"};
+	tagNode* next{ nullptr };
 };
 
-tagList* begin_node{ nullptr };
-tagList* end_node{ nullptr };
+tagNode* head_node = nullptr;
+tagNode* tail_node = nullptr;
+#pragma endregion
 
-void Add(const std::string& str)
+#pragma region "function list for simplex linked list"
+
+size_t GetListCount()
 {
-	if( begin_node == nullptr )
-	{
-		begin_node = new tagList( str );
-		end_node = begin_node;
-	}
-	else
-	{
-		tagList* new_node = new tagList( str );
-		end_node->next = new_node;
-		end_node = new_node;
-	}
-}
+	size_t list_count = 0;
 
-
-bool Remove(const std::string& str)
-{
-	if ( begin_node != nullptr && begin_node->str == str )
-	{
-		if (begin_node->next != nullptr )
-		{
-			tagList* next_node = begin_node->next;
-			delete begin_node;
-			begin_node = next_node;
-		}
-		else
-		{
-			delete begin_node;
-			begin_node = nullptr;
-		}
-
-		return true;
-	}
-
-	tagList* current_node = begin_node;
+	tagNode* current_node = head_node;
 
 	while ( current_node->next != nullptr )
 	{
-		if ( current_node->next->str == str )
-		{
-			tagList* delete_node = current_node->next;
-			current_node->next = current_node->next->next;
-			delete delete_node;
+		current_node= current_node->next;
+		list_count++;
+	}
 
-			return true;
+	return list_count;
+}
+
+void Add( std::string n )
+{
+	tail_node->next = new tagNode( std::move( n ) );
+	tail_node = tail_node->next;
+}
+
+tagNode* Find( std::string n )
+{
+	tagNode* current_node = head_node;
+
+	while ( current_node->next != nullptr )
+	{
+		if ( current_node->next->name == n )
+		{
+			return current_node->next;
 		}
 
 		current_node = current_node->next;
 	}
-	
-	return false;
+
+	return nullptr;
 }
 
-
-void RemoveAll()
+tagNode* Find( size_t index )
 {
-	tagList* current_node = begin_node;
+	size_t current_index = 0;
+	tagNode* current_node = head_node;
 
-	while ( current_node != nullptr )
+	while ( current_node->next != nullptr )
 	{
-		tagList* delete_node = current_node->next;
-		delete current_node;
-		current_node = delete_node;
+		if ( current_index == index )
+		{
+			return current_node->next;
+		}
+
+		current_node = current_node->next;
+		current_index++;
 	}
 
-	begin_node = nullptr;
-	end_node = nullptr;
-
-	printf( "Clear!\n" );
+	return nullptr;
 }
 
-void ShowData()
+tagNode* ReverseFind( size_t index )
 {
-	printf("=====================\n");
+	size_t max_count = GetListCount();
+	size_t loop_count = max_count - index - 1;
+	size_t current_index = 0;
 
-	tagList* current_node = begin_node;
+	tagNode* current_node = head_node;
+
+	while (current_node->next != nullptr )
+	{
+		if ( current_index == loop_count )
+		{
+			return current_node->next;
+		}
+
+		current_node = current_node->next;
+		current_index++;
+	}
+
+	return nullptr;
+}
+
+void Reverse()
+{
+	tagNode* current_node = head_node->next;
+	tagNode* two_prev_node = nullptr;
+	tagNode* prev_node = nullptr;
+
+	if ( head_node->next != nullptr )
+	{
+		tail_node = head_node->next;
+	}
+	
 	while ( current_node != nullptr )
 	{
-		printf( "%s\n", current_node->str.c_str() );
+		if ( current_node->next == nullptr )
+		{
+			head_node->next = current_node;
+		}
+
+		two_prev_node = prev_node;
+		prev_node = current_node;
+		current_node = current_node->next;
+		prev_node->next = two_prev_node;
+	}
+}
+
+void Remove( std::string n)
+{
+	tagNode* current_node = head_node;
+
+	while ( current_node->next != nullptr )
+	{
+		if ( current_node->next->name == n )
+		{
+			if ( current_node->next->next == nullptr )
+			{
+				tail_node = current_node;
+			}
+
+			tagNode* delete_node = current_node->next;
+			current_node->next = current_node->next->next;
+			delete delete_node;
+
+			return;
+		}
 
 		current_node = current_node->next;
 	}
-
-	printf( "=====================\n" );
 }
+
+void RemoveAll()
+{
+	std::cout << "===== RemoveAll =====" << std::endl;
+
+	tagNode* current_node = head_node;
+
+	while ( current_node->next != nullptr )
+	{
+		tagNode* delete_node = current_node->next;
+		current_node->next = current_node->next->next;
+		delete delete_node;
+	}
+
+	head_node->next = nullptr;
+	tail_node = head_node;
+
+	std::cout << "================" << std::endl;
+}
+
+void Show()
+{
+	std::cout << "===== Show =====" << std::endl;
+
+	tagNode* current_node = head_node;
+
+	while( current_node->next != nullptr )
+	{
+		std::cout << current_node->next->name << std::endl;
+		current_node = current_node->next;
+	}
+
+	std::cout << "================" << std::endl;
+}
+
+void ShowNode ( tagNode* node)
+{
+	if ( node == nullptr )
+	{
+		return;
+	}
+
+	std::cout << node->name << std::endl;
+}
+
+#pragma endregion
 
 int main()
 {
-	Add("Harmony");
-	Add( "Hooon_1" );
-	Add( "Hooon_2" );
-	Add( "Hooon_3" );
-	Add( "Hooon_4" );
-	Add( "Hooon_5" );
+#pragma region "Check memory leak"
+	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#pragma endregion
 
-	Remove( "Harmony" );
-	Remove("Hooon_1");
+#pragma region "Allocate memory"
+	head_node = new tagNode{};
+	tail_node = head_node;
+#pragma endregion
 
-	ShowData();
+	Add( "1" );
+	Add( "2" );
+	Add( "3" );
+	Add( "4" );
+	Add( "5" );
+	Show();
+
+	ShowNode( Find( 3 ) );
+	ShowNode( Find( "2" ) );
+	ShowNode( ReverseFind( 2 ) );
+
+	Reverse();
+	Show();
+
+	Remove("3");
+	Remove("1");
+	Show();
+
 	RemoveAll();
-	ShowData();
+	Show();
+
+#pragma region "Release"
+	RemoveAll();
+
+	if (head_node != nullptr )
+	{
+		delete head_node;
+		head_node = nullptr;
+	}
+#pragma endregion
 
 	return 0;
 }
